09931e87b4bd7d8b6709f210eae59778381232b6
lamineland <42746341+lamineland@users.noreply.github.com>
2022-12-25 00:24:06 +0000

    Template mounted

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,153 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title" key="t-menu">Menu</li>
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-home-circle"></i><span
+                            class="badge rounded-pill bg-info float-end">04</span>
+                        <span key="t-dashboards">Dashboards</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="index.html" key="t-default">Default</a></li>
+                        <li><a href="dashboard-saas.html" key="t-saas">Saas</a></li>
+                        <li><a href="dashboard-crypto.html" key="t-crypto">Crypto</a></li>
+                        <li><a href="dashboard-blog.html" key="t-blog">Blog</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-layout"></i>
+                        <span key="t-layouts">Layouts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow"
+                                key="t-vertical">Vertical</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="layouts-light-sidebar.html" key="t-light-sidebar">Light
+                                        Sidebar</a></li>
+                                <li><a href="layouts-compact-sidebar.html" key="t-compact-sidebar">Compact
+                                        Sidebar</a></li>
+                                <li><a href="layouts-icon-sidebar.html" key="t-icon-sidebar">Icon
+                                        Sidebar</a></li>
+                                <li><a href="layouts-boxed.html" key="t-boxed-width">Boxed Width</a></li>
+                                <li><a href="layouts-preloader.html" key="t-preloader">Preloader</a></li>
+                                <li><a href="layouts-colored-sidebar.html" key="t-colored-sidebar">Colored
+                                        Sidebar</a></li>
+                                <li><a href="layouts-scrollable.html" key="t-scrollable">Scrollable</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow"
+                                key="t-horizontal">Horizontal</a>
+                            <ul class="sub-menu" aria-expanded="true">
+                                <li><a href="layouts-horizontal.html" key="t-horizontal">Horizontal</a>
+                                </li>
+                                <li><a href="layouts-hori-topbar-light.html" key="t-topbar-light">Topbar
+                                        light</a></li>
+                                <li><a href="layouts-hori-boxed-width.html" key="t-boxed-width">Boxed
+                                        width</a></li>
+                                <li><a href="layouts-hori-preloader.html" key="t-preloader">Preloader</a>
+                                </li>
+                                <li><a href="layouts-hori-colored-header.html"
+                                        key="t-colored-topbar">Colored Header</a></li>
+                                <li><a href="layouts-hori-scrollable.html"
+                                        key="t-scrollable">Scrollable</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="menu-title" key="t-apps">Apps</li>
+
+
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <i class="bx bx-calendar"></i><span
+                            class="badge rounded-pill bg-success float-end">New</span>
+                        <span key="t-dashboards">Calendars</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="calendar.html" key="t-tui-calendar">TUI Calendar</a></li>
+                        <li><a href="calendar-full.html" key="t-full-calendar">Full Calendar</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="chat.html" class="waves-effect">
+                        <i class="bx bx-chat"></i>
+                        <span key="t-chat">Chat</span>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-briefcase-alt-2"></i>
+                        <span key="t-projects">Projects</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="projects-grid.html" key="t-p-grid">Projects Grid</a></li>
+                        <li><a href="projects-list.html" key="t-p-list">Projects List</a></li>
+                        <li><a href="projects-overview.html" key="t-p-overview">Project Overview</a></li>
+                        <li><a href="projects-create.html" key="t-create-new">Create New</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-task"></i>
+                        <span key="t-tasks">Tasks</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="tasks-list.html" key="t-task-list">Task List</a></li>
+                        <li><a href="tasks-kanban.html" key="t-kanban-board">Kanban Board</a></li>
+                        <li><a href="tasks-create.html" key="t-create-task">Create Task</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-user-detail"></i>
+                        <span key="t-contacts">Contacts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="contacts-grid.html" key="t-user-grid">Users Grid</a></li>
+                        <li><a href="contacts-list.html" key="t-user-list">Users List</a></li>
+                        <li><a href="contacts-profile.html" key="t-profile">Profile</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="waves-effect">
+                        <span class="badge rounded-pill bg-success float-end" key="t-new">New</span>
+                        <i class="bx bx-detail"></i>
+                        <span key="t-blog">Blog</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="blog-list.html" key="t-blog-list">Blog List</a></li>
+                        <li><a href="blog-grid.html" key="t-blog-grid">Blog Grid</a></li>
+                        <li><a href="blog-details.html" key="t-blog-details">Blog Details</a></li>
+                    </ul>
+                </li>
+
+
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
