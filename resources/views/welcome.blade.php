a4c562c3c1e687ddcd3785a6c9bc1c421f3e6336
lamineland <42746341+lamineland@users.noreply.github.com>
2022-12-25 16:37:16 +0000

    Footer OG

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -491,22 +491,7 @@
 
 
 
-                <footer class="footer">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <script>
-                                    document.write(new Date().getFullYear())
-                                </script> © Skote.
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="text-sm-end d-none d-sm-block">
-                                    Design & Develop by Themesbrand
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                @include('partials.footer')
             </div>
             <!-- end main content-->
 
